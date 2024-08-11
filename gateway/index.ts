878594b46343a3b206375aff04b04648ef8cd728
hi6okuni <hi6okuni@gmail.com>
2024-08-12 00:32:32 +0900

    feat: add gateway

---
+++ gateway/index.ts
@@ -0,0 +1,23 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloGateway, IntrospectAndCompose } from "@apollo/gateway";
+
+const gateway = new ApolloGateway({
+	supergraphSdl: new IntrospectAndCompose({
+		subgraphs: [
+			{ name: "reviews", url: "http://localhost:4001/query" },
+			// { name: "movies", url: "http://localhost:4002/query" },
+		],
+	}),
+});
+
+const server = new ApolloServer({
+	gateway,
+});
+
+const startServer = async () => {
+	const { url } = await startStandaloneServer(server);
+	console.log(`🚀 Server ready at ${url}`);
+};
+
+startServer();
